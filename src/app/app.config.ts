cc90c1c1b0d117572783ceded1fd25b77186ff06
Erik Rodriguez <erik@rodgz.com>
2026-02-05 09:05:11 -0300

    feat: add in-memory scrolling configuration to the Angular router.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,18 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter, withHashLocation } from '@angular/router';
+import { provideRouter, withHashLocation, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes, withHashLocation())
+    provideRouter(
+      routes,
+      withHashLocation(),
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'top',
+        anchorScrolling: 'enabled',
+      })
+    )
   ]
 };
